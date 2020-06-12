040194b45bb2bc1abc6ca681fbe364208dcd7807
Andrew Park <acpark123@gmail.com>
2020-06-12 07:19:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,7 +33,7 @@ def get_english_meaning (file_path, emoticon)
   emoticon_library.each do | key, value |
     value.each do | lang, emo |
       if lang == :japanese && emo == emoticon
-         key
+         return key
       end   
     end
   end
