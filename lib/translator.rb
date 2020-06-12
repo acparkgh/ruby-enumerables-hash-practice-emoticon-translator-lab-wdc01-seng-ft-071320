42632e514d2a2d537d6af6a5916aa0f173462f88
Andrew Park <acpark123@gmail.com>
2020-06-12 05:42:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(yaml_file)
   
   emoticons_library = {}
   library.each do | key, value_array |
-      emoticon_library[key] = {}
+      emoticons_library[key] = {}
     value_array.each do | emoticon |
       emoticons_library[key][:english] = emoticon[0]
       emoticons_library[key][:japanese] = emoticon[1]
