518d8a3b63795219386788c780cc92f42357f750
Andrew Park <acpark123@gmail.com>
2020-06-12 08:06:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,7 @@ def get_japanese_emoticon(file_path, western_emoticon)
     end
     
   end
-  japanese_translation = nil ? 
+  japanese_translation = nil ? "Sorry, that emoticon was not found" : japanese_translation
 end
 
 
