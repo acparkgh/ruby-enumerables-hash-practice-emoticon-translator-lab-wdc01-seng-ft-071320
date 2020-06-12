53ea0e8aa5c4e42970d51726e924ef4727e576d2
Andrew Park <acpark123@gmail.com>
2020-06-12 07:22:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,7 @@ def get_english_meaning (file_path, emoticon)
     value.each do | lang, emo |
       if lang == :japanese && emo == emoticon
          return key
-      elsif
+      else
          return "Sorry, that emoticon was not found"
       end   
     end
